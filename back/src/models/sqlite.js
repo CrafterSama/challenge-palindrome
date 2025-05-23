893d846468ab0aac93b61fcedf82f9f00907a3cc
Julmer Olivero <jolivero.03@gmail.com>
2025-05-23 14:07:40 +0000

    feat: add sqlite, database, and script command to create the database

---
+++ back/src/models/sqlite.js
@@ -0,0 +1,26 @@
+import path from "path";
+import sqlite from "sqlite3";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const db = new sqlite.Database(
+  path.resolve(__dirname, "../db/challenge.db"),
+  (error) => {
+    if (error) {
+      return console.error(error);
+    }
+
+    const sql = `CREATE TABLE IF NOT EXISTS challenge (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        words VARCHAR(255) NOT NULL,
+      )`;
+    db.run(sql, (error) => {
+      if (error) {
+        return console.error(error);
+      }
+      console.log("Table created successfully");
+    });
+  }
+);
